7cfab50cc744111373e1f9fdd3e68b9d99726c2f
JOSE ABADIE <JOSE.ABADIE@ucu.edu.uy>
2024-08-12 10:35:08 -0300

    comentario en Program sobre estado de situación del profe

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -4,7 +4,7 @@ using Ucu.Poo.GitChallenge;
 
 Console.WriteLine(Addition.Add(7, 2));
 Console.WriteLine(Substraction.Substract(10, 1));
-/*Console.WriteLine(Multiplication.Multiply(3, 3));
-Console.WriteLine(Division.Divide(18, 2));  */
+Console.WriteLine(Multiplication.Multiply(3, 3));
+Console.WriteLine(Division.Divide(18, 2)); 
 
-    
+Console.WriteLine("Me siento muy mal y me quiero ir a acostar..."); 
