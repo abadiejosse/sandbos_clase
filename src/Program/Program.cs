5a58c7e0f15e9187515444b1e5211fe5d439a9e6
Gabriel Felipe <gabriel.felipe@correo.ucu.edu.uy>
2024-08-12 10:34:08 -0300

    Comentario de Gabriel

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,4 +7,7 @@ Console.WriteLine(Substraction.Substract(10, 1));
 /*Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));  */
 
+
+// Comentario de Gabriel
+
     
