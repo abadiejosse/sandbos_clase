1af7e9790ad446f19f4a454bfa58ed8ed18ca7d6
Gabriel Felipe <gabriel.felipe@correo.ucu.edu.uy>
2024-08-12 10:11:05 -0300

    Cambios de Gabriel

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -6,4 +6,7 @@ Console.WriteLine(Addition.Add(7, 2));
 Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
+
+// Cambios de Gabriel
+Console.WriteLine("Hola, esto lo escribió Gabriel :)");
     
